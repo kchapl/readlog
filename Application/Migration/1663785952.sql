8e609eb7ff993ed92086166117fdd460b4ebf138
K <kchapl@users.noreply.github.com>
2022-09-21 20:46:22 +0100

    Add books table

--- Application/Migration/1663785952.sql
+++ Application/Migration/1663785952.sql
@@ -1,4 +1,3 @@
--- Your database schema. Use the Schema Designer at http://localhost:8001/ to add some tables.
 CREATE TABLE books (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     author TEXT NOT NULL,
